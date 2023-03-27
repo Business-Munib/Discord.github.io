87003aa8133fe74c79ab393a54aad8a0b01ced3a
Munib36 <mr.munib@mail.ru>
2023-03-27 19:47:41 +0200

    TIC TAC TOE

--- practice/script.js
+++ practice/script.js
@@ -1,38 +1,89 @@
-const label = document.querySelector("#label")
-const add = document.querySelector("#add")
-const take = document.querySelector("#take")
-let count;
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = 'X';
+const cells = document.querySelectorAll(".box")
+const resetBtn = document.getElementById("reset")
+let running = false;
+let winnings = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
 
-label.innerHTML = getCookie("value")
+  [0,3,5],
+  [1,4,7],
+  [2,5,8],
 
+  [0,4,8],
+  [2,4,6]
+]
 
-console.log(getCookie("value"))
+startGame();
 
-add.addEventListener("click", increment)
-take.addEventListener("click", decrement)
+function startGame(){
+	if(running == false){
+		running = true
+		cells.forEach(cell => cell.addEventListener("click", cellClicked))
+		resetBtn.addEventListener("click", resetGame)
+	}
 
-function increment() {
-	count = getCookie("value") + 1;
-	document.cookie = `value=${count}; expires=Sun, 1, January, 2060 12:00:00 UTC; path=/`
-	label.textContent = getCookie("value")
+}
+function cellClicked() {
+	const cellIndex = this.getAttribute("cellIndex");
+
+	if(options[cellIndex] != "" || !running){
+		return;
+	}
+	updateCells(this, cellIndex);
+	checkWin();
+
 }
 
-function decrement() {
-	count = getCookie("value") - 1;
-	document.cookie = `value=${count}; expires=Sun, 1, January, 2060 12:00:00 UTC; path=/`
-	label.textContent = getCookie("value")
+function updateCells(cell, index) {
+  // TODO: update the board and the screen to reflect the player's move
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+
+
 }
 
+function checkWin() {
+  // TODO: check if the game has been won or if it's a tie
+  let roundWon = false;
 
-function getCookie(name) {
-	const cDecode = decodeURIComponent(document.cookie);
-	const cArray = cDecode.split("; ")
-	let result = null;
+  for(let i = 0; i < winnings.length; i++){
+  	  const condition = winnings[i];
 
-	cArray.forEach(element => {
-		if(element.indexOf(name) == 0){
-			result = Number(element.substring(name.length + 1))
-		}
-	})
-	return result;
+  	  const cellA = options[condition[0]];
+  	  const cellB = options[condition[1]];
+  	  const cellC = options[condition[2]];
+
+  	  if(cellA == "" || cellB == "" || cellC == ""){
+  	  	  continue;
+  	  }
+  	  if(cellA == cellB && cellB == cellC){
+  	  	  roundWon = true
+  	  	  break;
+  	  }
+  }
+  if(roundWon){
+  	  alert(`Omg ${currentPlayer} WON`)
+  	  running = false;
+  }else if(!options.includes("")){
+  	  alert("draw bruh")
+  	  running = false;
+  }else{
+  	  switchPlayer();
+  }
+}
+
+function switchPlayer() {
+  // TODO: switch the current player to the other player
+	currentPlayer = (currentPlayer === "X") ? "O" : "X";
+}
+
+function resetGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
